2d5f94903b2e4b1c5553e7d01dc4de17665881ca
Chris <chrishwang@live.cn>
2019-06-01 18:12:22 +0800

    init

---
+++ FaceDetectionCuda/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using FaceRecognitionDotNet;
+
+namespace FaceDetectionCuda
+{
+    class Program
+    {
+        private static FaceRecognition _faceRecognition;
+
+        static void Main(string[] args)
+        {
+            var modelDirectory = @"D:\dev\dlib-model";
+            _faceRecognition = FaceRecognition.Create(modelDirectory);
+
+            var src = @"input.jpg";
+            using (var imageSrc = FaceRecognition.LoadImageFile(src))
+            {
+                var faceLocations = _faceRecognition.FaceLocations(imageSrc, 0, Model.Cnn).ToArray();
+                var faceEncodings = _faceRecognition.FaceEncodings(imageSrc, faceLocations).ToArray();
+                Console.WriteLine($"Face count: {faceEncodings.Length}");
+            }
+        }
+    }
+}
